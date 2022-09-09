15c28eb591d33f6b6bf2fcd8e859fa7d976b2354
Wayne Zeng <wayne@gimi65536.xyz>
2022-09-09 18:40:43 +0900

    Basic implementation for server state This implementation focuses on flexibility of keys, so the object type should be controlled by developers.

---
+++ discord/server_state.py
@@ -0,0 +1,23 @@
+from threading import Lock
+from load_db import db
+from typing import Dict
+
+class _State:
+	def __init__(self):
+		# To ensure that each update is handled by mutex lock,
+		# the "object" part is recommended to be immutable.
+		self._real_dict: Dict[str, Tuple[Lock, object]] = {}
+	def get(self, key: str):
+		return self._real_dict[key][1]
+	def set(self, key: str, obj: object):
+		if key not in self._real_dict:
+			self._real_dict[key] = (Lock(), obj)
+		else:
+			lock = self._real_dict[key][0]
+			with lock:
+				self._real_dict[key] = (lock, obj)
+
+# Importing is already threading-safe in python, so we don't need to concern this:
+state = _State()
+
+__all__ = ['state']
