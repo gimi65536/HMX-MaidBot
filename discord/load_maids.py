d38176672c22d4bf0d52b75f71ccee942a037c69
Wayne Zeng <wayne@gimi65536.xyz>
2022-09-09 20:31:51 +0900

    Make information of maids immutable

--- discord/load_maids.py
+++ discord/load_maids.py
@@ -1,9 +1,13 @@
 from load_db import db
 from maid import Maid
 from pymongo import ASCENDING
+from types import MappingProxyType
 
 # The maids information is loaded once per execution.
 _maids = list(db['maid-list'].find("_id", ASCENDING))
-maids = [Maid(m['name'], m['display_name'], m['avatar']) for m in _maids]
+maids = tuple(Maid(m['name'], m['display_name'], m['avatar']) for m in _maids)
+# This dict provides a way to retrieve maids by names.
+# Also, with the features of dict, the maid order is kept.
+maids_dict = MappingProxyType({m.name: m for m in maids})
 
-__all__ = ['maids']
+__all__ = ['maids', 'maids_dict']
