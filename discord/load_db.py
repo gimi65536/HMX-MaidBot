a9e11a860a2343eb7079865237d6a1774d22fd46
Wayne Zeng <wayne@gimi65536.xyz>
2022-09-10 14:23:27 +0900

    Define mongodb address in secret file

--- discord/load_db.py
+++ discord/load_db.py
@@ -1,7 +1,7 @@
 import pymongo
 from load_secrets import secret
 
-mongo = pymongo.MongoClient(f"mongodb://{secret['mongo_admin']}:{secret['mongo_pwd']}@mongodb")
+mongo = pymongo.MongoClient(f"mongodb://{secret['mongo_admin']}:{secret['mongo_pwd']}@{secret['mongo_address']}")
 db = mongo['maid-bot']
 
 __all__ = ['db']
