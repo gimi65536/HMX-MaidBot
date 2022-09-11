d714ac277c0536695ace978437ccc0f168bdfb82
Wayne Zeng <wayne@gimi65536.xyz>
2022-09-11 14:23:03 +0900

    Make secret .env in development

--- discord/load_secrets.py
+++ discord/load_secrets.py
@@ -1,7 +1,13 @@
-import json
+from decouple import config
 
-# load secret information
-with open('secret.json') as f:
-	secret = json.load(f)
+secret = {
+	"app_id": config('APP_ID', cast = int),
+	"app_client_secret": config('APP_CLIENT_SECRET'),
+	"bot_token": config('BOT_TOKEN'),
+	"mongo_address": config('MONGO_ADDRESS'),
+	"mongo_admin": config('MONGO_ADMIN'),
+	"mongo_pwd": config('MONGO_PWD'),
+	"debug_server_id": config('DEBUG_SERVER_ID', cast = int)
+}
 
 __all__ = ['secret']
