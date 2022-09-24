91ed207f2d8ab732c8d460c3b2e15f2d7c933e4f
Wayne Zeng <wayne@gimi65536.xyz>
2022-09-24 22:30:10 +0900

    Dice games with different faces

--- discord/dcmaid/ext/ext-roll/dice.py
+++ discord/dcmaid/ext/ext-roll/dice.py
@@ -13,3 +13,34 @@ class DiscordDiceGame(DiscordRollGame, DiceGame):
 
 	async def _process(self, i: int):
 		return int_to_emoji(i)
+
+class DiscordDiceNGame(DiscordRollGame, DiceGame):
+	options = {
+		0: []
+	}
+	N: int
+
+	def __init__(self, ctx, webhook, arguments, random, initial_text = None, **kwargs):
+		DiscordRollGame.__init__(ctx, webhook, arguments, initial_text, kwargs)
+		DiceGame.__init__(self.N, random)
+
+	async def _process(self, i: int):
+		return int_to_emoji(i)
+
+class DiscordDice4Game(DiscordDiceNGame):
+	N = 4
+
+class DiscordDice6Game(DiscordDiceNGame):
+	N = 6
+
+class DiscordDice8Game(DiscordDiceNGame):
+	N = 8
+
+class DiscordDice10Game(DiscordDiceNGame):
+	N = 10
+
+class DiscordDice12Game(DiscordDiceNGame):
+	N = 12
+
+class DiscordDice20Game(DiscordDiceNGame):
+	N = 20
