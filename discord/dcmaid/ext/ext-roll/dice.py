08c10ad45820625ec6db1d797d48a08e6a5a5bc0
Wayne Zeng <wayne@gimi65536.xyz>
2022-09-25 20:00:31 +0900

    Improve dice game initialization Since some ugly codes are not expected to be used outside, we eliminate some unnecessary checks here.

--- discord/dcmaid/ext/ext-roll/dice.py
+++ discord/dcmaid/ext/ext-roll/dice.py
@@ -7,28 +7,26 @@ class DiscordDiceGame(DiscordDigitRollGame, DiceGame):
 		DiscordDigitRollGame.__init__(self, ctx, webhook, arguments, initial_text, kwargs)
 		DiceGame.__init__(self, self.processed_kwargs['faces'], random)
 
-class DiscordDiceNGame(DiscordDigitRollGame):
+class _DiscordDiceNGame(DiscordDigitRollGame):
+	# To activate this __init__, please make this class the first parent class
 	def __init__(self, ctx, webhook, arguments, random, initial_text = None, **kwargs):
 		DiscordDigitRollGame.__init__(self, ctx, webhook, arguments, initial_text, kwargs)
-		for base in type(self).__bases__:
-			if issubclass(base, DiceNGame):
-				base.__init__(self, random)
-				return
+		type(self).__bases__[1].__init__(self, random)
 
-class DiscordDice4Game(DiscordDiceNGame, Dice4Game):
+class DiscordDice4Game(_DiscordDiceNGame, Dice4Game):
 	pass
 
-class DiscordDice6Game(DiscordDiceNGame, Dice6Game):
+class DiscordDice6Game(_DiscordDiceNGame, Dice6Game):
 	pass
 
-class DiscordDice8Game(DiscordDiceNGame, Dice8Game):
+class DiscordDice8Game(_DiscordDiceNGame, Dice8Game):
 	pass
 
-class DiscordDice10Game(DiscordDiceNGame, Dice10Game):
+class DiscordDice10Game(_DiscordDiceNGame, Dice10Game):
 	pass
 
-class DiscordDice12Game(DiscordDiceNGame, Dice12Game):
+class DiscordDice12Game(_DiscordDiceNGame, Dice12Game):
 	pass
 
-class DiscordDice20Game(DiscordDiceNGame, Dice20Game):
+class DiscordDice20Game(_DiscordDiceNGame, Dice20Game):
 	pass
