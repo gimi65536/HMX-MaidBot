dbe4b79a5f8d0c3ad94453c63702ec921fe09264
Wayne Zeng <wayne@gimi65536.xyz>
2022-10-02 15:16:15 +0200

    Fix _verbose_argiter that belongs to actual games instead of abstract ones

--- common/rollgames/choose.py
+++ common/rollgames/choose.py
@@ -10,9 +10,6 @@ class AbstractSampleGame(BaseRollGame):
 		self.choices = l
 		self.random = random
 
-	def _verbose_argiter(self):
-		return (' '.join(f'`{c}`' for c in self.choices), )
-
 	async def _process(self, s: str):
 		return f'**{s}**'
 
--- discord/dcmaid/ext/ext-roll/choose.py
+++ discord/dcmaid/ext/ext-roll/choose.py
@@ -1,15 +1,19 @@
 from . import DiscordRollGame
 from rollgames.choose import *
 
-class DiscordChooseGame(DiscordRollGame, ChooseGame, reg = True):
+class _DiscordSampleGame(DiscordRollGame):
+	def _verbose_argiter(self):
+		return (' '.join(f'`{c}`' for c in self.choices), )
+
+class DiscordChooseGame(_DiscordSampleGame, ChooseGame, reg = True):
 	def __init__(self, ctx, webhook, arguments, random, initial_text = None, **kwargs):
-		DiscordRollGame.__init__(self, ctx, webhook, arguments, initial_text, kwargs)
+		_DiscordSampleGame.__init__(self, ctx, webhook, arguments, initial_text, kwargs)
 		ChooseGame.__init__(self, *self.processed_kwargs.values(), random)
 
-class _DiscordSampleNGame(DiscordRollGame):
+class _DiscordSampleNGame(_DiscordSampleGame):
 	# To activate this __init__, please make this class the first parent class
 	def __init__(self, ctx, webhook, arguments, random, initial_text = None, **kwargs):
-		DiscordRollGame.__init__(self, ctx, webhook, arguments, initial_text, kwargs)
+		_DiscordSampleGame.__init__(self, ctx, webhook, arguments, initial_text, kwargs)
 		type(self).__bases__[1].__init__(self, *self.processed_kwargs.values(), random)
 
 class DiscordChooseMultiGame(_DiscordSampleNGame, ChooseMultiGame, reg = True):
