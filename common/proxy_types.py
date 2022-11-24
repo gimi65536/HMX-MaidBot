86afb4cfd78c2ce2743d4f1a45fa841454d0c015
Wayne Zeng <wayne@gimi65536.xyz>
2022-11-24 22:44:20 +0900

    Fix proxy types Since MappingProxyType cannot have subclasses, we re-implemented a python class _MappingProxyType fitting the built-in MappingProxyType and use it to construct subclasses. Note that _MappingProxyType is not designed for direct use due to the performance but we need the subclasses to have the functionalities of proxy and the features of the other mapping classes such as Counter.

--- common/proxy_types.py
+++ common/proxy_types.py
@@ -1,18 +1,53 @@
 from collections import Counter
 from types import MappingProxyType
+from typing import Mapping
 
-class CounterProxyType(MappingProxyType):
+# This is a remake of the built-in type MappingProxyType.
+# It is not recommended to use this directly since there
+# is the built-in version and that performs better.
+class _MappingProxyType:
+	def __init__(self, mapping: Mapping):
+		self._d = mapping
+
+	def __contains__(self, item):
+		return self._d.__contains__(item)
+
+	def __getitem__(self, key):
+		return self._d.__getitem__(key)
+
+	def __iter__(self):
+		return self._d.__iter__()
+
+	def __len__(self):
+		return self._d.__len__()
+
+	def copy(self):
+		return self._d.copy()
+
+	def get(self, key, default = None):
+		return self._d.get(key, default)
+
+	def items(self):
+		return self._d.items()
+
+	def keys(self):
+		return self._d.keys()
+
+	def values(self):
+		return self._d.values()
+
+	def __reversed__(self):
+		return self._d.__reversed__()
+
+class CounterProxyType(_MappingProxyType):
 	def __init__(self, counter: Counter):
 		super().__init__(counter)
-		# Seems that we cannot get the origin dict stored in MappingProxyType,
-		# so we need to store it manually.
-		self._c = counter
 
 	def elements(self):
-		return self._c.elements()
+		return self._d.elements()
 
 	def most_common(self, n = None):
-		return self._c.most_common(n)
+		return self._d.most_common(n)
 
 	def total(self):
-		return self._c.total()
+		return self._d.total()
