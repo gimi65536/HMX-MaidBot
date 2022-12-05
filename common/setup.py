ebae82c4b84884e6b09a88114dd8fe8a19d2a00c
Wayne Zeng <wayne@gimi65536.xyz>
2022-12-05 23:46:11 +0900

    Use setup.py instead of PYTHONPATH to get the common modules

---
+++ common/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name = 'common',
+	version = '0.4',
+	description = 'Common utilities used in HMX-MaidBot project',
+	packages = ['rollgames', 'simple_parsers'],
+	py_modules = ['proxy_types', 'reader'],
+	install_requires = [
+		'more_itertools>=9',
+		'pyparsing>=3.0.9'
+	]
+)
