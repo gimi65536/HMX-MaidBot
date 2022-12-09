1539e0251461de20bd443d2769e95c6a8653af16
Wayne Zeng <wayne@gimi65536.xyz>
2022-12-09 15:26:25 +0900

    Remove calcs package in the common setup.py

--- common/setup.py
+++ common/setup.py
@@ -4,11 +4,10 @@ setup(
 	name = 'common',
 	version = '0.4',
 	description = 'Common utilities used in HMX-MaidBot project',
-	packages = ['rollgames', 'simple_parsers', 'calcs'],
+	packages = ['rollgames', 'simple_parsers'],
 	py_modules = ['proxy_types', 'reader'],
 	install_requires = [
 		'more_itertools>=9',
 		'pyparsing>=3.0.9',
-		'sympy>=1.11.1'
 	]
 )
